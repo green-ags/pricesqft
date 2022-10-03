2793c4366316209f3ba728ee89e1765959eb72e1
[hector-sanchez] <[perfectgreenco@gmail.com]>
2022-10-03 16:47:31 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let oasis = 1.15;
+let sunrise = 1.35;
+let evergreen = 1.50;
+
+let grass;
+switch (grass) {
+	case oasis:
+		// code block
+		break;
+	case sunrise:
+		// code block
+		break;
+	case evergreen:
+		// code block
+		break;
+	default:
+	// code block
+}
